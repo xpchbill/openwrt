01ef6c7d87cfb1b24e99e7f79eab102462247833
Bill Xiong <xpchbill8@gmail.com>
2024-07-11 09:28:16 +0800

    Initial commit

---
+++ diy-part2.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.30.1/g' package/base-files/files/bin/config_generate
+
+
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/themes/luci-theme-design
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git feeds/luci/themes/luci-theme-argon
+git clone https://github.com/gngpp/luci-theme-design.git feeds/luci/themes/luci-theme-design
+
+rm -rf feeds/luci/applications/luci-app-argon-config
+rm -rf feeds/luci/applications/luci-app-design-config
+git clone -b 18.06 https://github.com/jerrykuku/luci-app-argon-config.git feeds/luci/applications/luci-app-argon-config
+git clone https://github.com/gngpp/luci-app-design-config.git feeds/luci/applications/luci-app-design-config
+
+git clone -b master --single-branch --filter=blob:none https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+
+#sed -i "s/luci-theme-bootstrap/luci-theme-argon/g" feeds/luci/collections/luci/Makefile
+sed -i "s/luci-theme-bootstrap/luci-theme-design/g" feeds/luci/collections/luci/Makefile
+
+#sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' target/linux/x86/Makefile
+
+#ddns-go
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
+
+#lucky
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
+
+#dockerman
+git clone https://github.com/blacktitty-y/luci-app-dockerman.git
+
+#netdata
+git clone https://github.com/sirpdboy/luci-app-netdata package/luci-app-netdata
+
+#netspeedtest
+git clone https://github.com/sirpdboy/netspeedtest.git package/netspeedtest
+
+
+
